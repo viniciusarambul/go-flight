86bc55f7389f58aa09fcaae0ff959fa43c589c0d
Vinicius <vinicius.vieira@hotmail.com>
2022-02-12 03:01:20 -0300

    fix: retirando comentarios

--- app/infrastructure/repository/plane_mysql.go
+++ app/infrastructure/repository/plane_mysql.go
@@ -39,20 +39,5 @@ func (p PlaneMySQLRepository) Select(plane entity.Plane) error {
 	}
 
 	return nil
-	/*switch {
-	case err == sql.ErrNoRows:
-		log.Printf("no user with id %d\n", id)
-	case err != nil:
-		log.Fatalf("query error: %v\n", err)
-	default:
-		log.Printf("id is %d, modelo: %v, descricao: %v, status:%v\n", &plane.ID, &plane.Model, &plane.Description, &plane.Status)
-	}
-	/*
-		for result.Next() {
-			var plane entity.Plane
-			err = result.Scan(&plane.ID, &plane.Model, &plane.Description, &plane.Status)
-		}
-		log.Printf(plane.ID)
-	*/
 
 }
